c3162e0e51a040ce44c415766cfb151b7628dfbf
amency <nikitapelagecha@gmail.com>
2024-09-09 01:39:53 +0100

    achieved 75% but it plateaus again

--- train.py
+++ train.py
@@ -32,19 +32,19 @@ model = Model(input_size=settings["input_size"],
 # Hyperparameters
 batch_size = 256                                              # Number of samples per batch
 lr = 0.001                                                    # Learning rate for the optimizer
-num_epochs = 25                                               # Total number of epochs for training
+num_epochs = 200                                              # Total number of epochs for training
 
 # Loss Function
 criterion = nn.CrossEntropyLoss()                             # Loss function for multi-class classification tasks
 
 # Optimizer
-optimiser = torch.optim.Adam(model.parameters(), lr=lr)       # AdamW optimizer with weight decay for regularization
+optimiser = torch.optim.AdamW(model.parameters(), lr=lr)       # AdamW optimizer with weight decay for regularization
 
 # Learning Rate Scheduler
 # scheduler = torch.optim.lr_scheduler.StepLR(optimiser, 
 #                                              step_size=5,     # Frequency (in epochs) to update the learning rate
 #                                              gamma=0.1)       # Factor by which the learning rate is reduced
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimiser, mode='min', factor=0.1, patience=3, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimiser, mode='min', factor=0.5, patience=3, threshold=0.5, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08)
 
 # Model Paths
 model_path, accuracy_path = helpers.model_dirs(model.model_name(), dataset_name)  # Paths for saving model and accuracy
@@ -103,7 +103,8 @@ for epoch in range(num_epochs):
             pbar.update(1)
             pbar.set_postfix({'Avg Loss': f'{average_loss:.4f}'})
             current_lr = scheduler.get_last_lr()[0]  # Get the first element if you have a single parameter group
-            pbar.set_postfix({'LR': f'{current_lr:.4f}'})
+            # pbar.set_postfix({'LR': f'{current_lr:.4f}'})
+            pbar.set_postfix({'LR': f'{scheduler.get_last_lr()[0]:.4f}', 'Loss': f'{loss:.4f}'})
 
     # Compute average loss for the epoch
     epoch_loss = running_loss / len(train_dataset)
