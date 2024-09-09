aa1c97e96f53f00b43b49d0d675d379708cb4c71
amency <nikitapelagecha@gmail.com>
2024-09-09 02:38:16 +0100

    still 75%

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ model = Model(input_size=settings["input_size"],
 # Hyperparameters
 batch_size = 256                                              # Number of samples per batch
 lr = 0.001                                                    # Learning rate for the optimizer
-num_epochs = 20                                              # Total number of epochs for training
+num_epochs = 200                                              # Total number of epochs for training
 
 # Loss Function
 criterion = nn.CrossEntropyLoss()                             # Loss function for multi-class classification tasks
